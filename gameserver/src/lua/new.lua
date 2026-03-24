2b369efd2c0c45d14ce09ca4211246d8c6359c27
mikugirls <mikugirls@163.com>
2026-03-24 11:12:47 +0800

    Update new.lua

--- gameserver/src/lua/new.lua
+++ gameserver/src/lua/new.lua
@@ -1,10 +1,1 @@
---local function onDialogClosed()
-    --CS.UnityEngine.Application.OpenURL("https://discord.gg/CastoricePS")
---end
 
-local function show_hint()
-    local text = ""
-    CS.RPG.Client.ConfirmDialogUtil.ShowCustomOkCancelHint(text, onDialogClosed)
-end
-
-show_hint()
